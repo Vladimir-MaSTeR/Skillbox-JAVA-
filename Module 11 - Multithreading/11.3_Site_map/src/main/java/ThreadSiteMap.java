1f1e603b8e80516aa5ab1d7429abbff3acb5e941
Vladimir <tsyuman87binary@gmail.com>
2019-11-28 02:48:24 +0700

    Modified 11.3

--- Module 11 - Multithreading/11.3_Site_map/src/main/java/ThreadSiteMap.java
+++ Module 11 - Multithreading/11.3_Site_map/src/main/java/ThreadSiteMap.java
@@ -7,6 +7,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.TreeSet;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CopyOnWriteArraySet;
 
@@ -20,32 +23,41 @@ public class ThreadSiteMap implements Runnable {
         this.writeFile = writeFile;
     }
 
-    private static CopyOnWriteArraySet<String> allLinks = new CopyOnWriteArraySet<>();
+//   private static CopyOnWriteArraySet<String> allLinks = new CopyOnWriteArraySet<>();
+   private Collection<String> linksDone = Collections.synchronizedCollection(new TreeSet<>());
     @Override
     public void run() {
         try {
             while (queue.size() > 0) {
-                if ( queue.poll() !=  null) {
-                    Document document = Jsoup.connect(String.valueOf(queue)).maxBodySize(0).get();
+                String url = queue.poll();
+                if ( url !=  null) {
+                    Document document = Jsoup.connect(url).maxBodySize(0).get();
                     Elements elements = document.select("a");
 
                     elements.forEach(el -> {
                         String attr = el.attr("abs:href");
                         if (!attr.isEmpty() && attr.startsWith(String.valueOf(queue)) &&
-                                !allLinks.contains(attr) && !attr.contains("#")) {
-                            try {
-                                Path path = Paths.get(writeFile);
-                                Files.writeString(path, attr, StandardOpenOption.APPEND);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
+                                !linksDone.contains(attr) && !attr.contains("#")) {
+                            linksDone.add(attr);
                           //  allLinks.add(attr);
                         }
                     });
                 }
             }
+            write(linksDone);
         } catch (IOException ex) {
             ex.printStackTrace();
+        }
+    }
+
+    public void write(Collection<String> linksDone) {
+        for (String link : linksDone) {
+            try {
+                Path path = Paths.get(writeFile);
+                Files.writeString(path, link, StandardOpenOption.APPEND);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
