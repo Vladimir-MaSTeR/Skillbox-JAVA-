04c5dd85e60aac7152463786e716c5f4e7670575
Vladimir <tsyuman87binary@gmail.com>
2019-12-06 22:34:54 +0700

    create 12.4

---
+++ Module 12 - Web application development (Spring Boot)/12.4-ToDoList - View/src/main/java/main/ToDoController.java
@@ -0,0 +1,83 @@
+package main;
+
+
+import main.model.ToDo;
+import main.model.ToDoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class ToDoController {
+
+    @Autowired(required=false)
+    private ToDoRepository toDoRepository;
+
+    @RequestMapping(value = "/ToDo", method = RequestMethod.GET)
+    public List<ToDo> list() {
+        Iterable<ToDo> toDoIterable = toDoRepository.findAll();
+        ArrayList<ToDo> toDoArrayList = new ArrayList<>();
+
+        for (ToDo toDo : toDoIterable) {
+            toDoArrayList.add(toDo);
+        }
+
+        return toDoArrayList;
+    }
+
+    @RequestMapping(value = "/ToDo", method = RequestMethod.POST)
+    public int add(ToDo toDo) {
+        ToDo newToDo = toDoRepository.save(toDo);
+        return newToDo.getId();
+    }
+
+    @RequestMapping(value = "/ToDo/{id}", method = RequestMethod.GET)
+    public ResponseEntity get (@PathVariable int id) {
+
+        Optional<ToDo> optionalToDo = toDoRepository.findById(id);
+
+        if (!optionalToDo.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return new ResponseEntity(optionalToDo.get(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/ToDo/{id:\\d+}", method = RequestMethod.DELETE)
+    public ResponseEntity delete(@PathVariable int id) {
+        if (!toDoRepository.findById(id).isPresent())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+
+        toDoRepository.deleteById(id);
+        return new ResponseEntity(id, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/ToDo/{id}", method = RequestMethod.PUT)
+    public ResponseEntity put(@PathVariable int id) {
+        Optional<ToDo> optionalToDo = toDoRepository.findById(id);
+
+        if (!optionalToDo.isPresent()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+
+        return new ResponseEntity(toDoRepository.save(optionalToDo.get()), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/ToDo/search/{name}", method = RequestMethod.GET)
+    public ResponseEntity  search(@PathVariable String name) {
+        Iterable<ToDo> toDoIterable = toDoRepository.findAll();
+        ArrayList<ToDo> toDoArrayList = new ArrayList<>();
+
+            for (ToDo toDo : toDoIterable) {
+                if (toDo.getName().equals(name)) {
+                    toDoArrayList.add(toDo);
+                }
+            }
+        return new ResponseEntity(toDoArrayList, HttpStatus.OK);
+    }
+}
---
+++ Module 12 - Web application development (Spring Boot)/12.4-ToDoList - View/src/main/resources/static/js/main.js
@@ -0,0 +1,69 @@
+$(function() {
+
+      appendToDo = function(data) {
+       var todoCode = '<a href="#" class="ToDo-link" data-id="' + data.id + '">' + data.name + '</a><br>';
+       $('#ToDo list').append('<div>' + todoCode + '</div>');
+     };
+
+     //
+//     $.get('/ToDo/', function(response) {
+//        for(i in response) {
+//        appendToDo(response[i]);
+//        }
+//     });
+
+     //
+     $('#add-new-list').click(function() {
+          $('#ToDo-form').css('display', 'flex');
+      });
+
+     //
+     $('#ToDo-form').click(function(event) {
+        if(event.target === this) {
+           $(this).css('display', 'none');
+        }
+     });
+
+     $(document).on('click', '.ToDo-link', function() {
+         var link = $(this);
+         var toDoId = link.data('id');
+         $.ajax({
+                    method: "GET",
+                    url: '/ToDo/' + toDoId,
+                    //data: data,
+                    success: function(response) {
+                       var code = '<span>Год выпуска ' + response.details + '</span>';
+                       link.parent().append(code);
+                    },
+                    error: function(response) {
+                        if(response.status == 404) {
+                           alert('Книга не найдена!');
+                        }
+                    }
+                 });
+                 return false;
+     });
+
+     //
+     $('#Save-list').click(function() {
+        var data = $('ToDo-form form').serialize();
+        $.ajax({
+           method: "POST",
+           url: '/toDo/',
+           //data: data,
+           success: function(response) {
+              $('ToDo-form').css('display', 'none');
+              var todo = {};
+              todo.id = response;
+              var dataArray = $('#ToDo-form form').serializeArray();
+
+                for(i in dataArray) {
+                   todo[dataArray[i]['name']] = dataArray[i]['value'];
+                }
+                appendToDo(todo);
+           }
+        });
+        return false;
+     });
+
+})
