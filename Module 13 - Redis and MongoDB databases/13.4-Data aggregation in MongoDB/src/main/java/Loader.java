bc788d599a2e30b4743f170d5777edfcff288300
Vladimir <tsyuman87binary@gmail.com>
2020-01-09 00:57:43 +0700

    modified 13.4 - 2

--- Module 13 - Redis and MongoDB databases/13.4-Data aggregation in MongoDB/src/main/java/Loader.java
+++ Module 13 - Redis and MongoDB databases/13.4-Data aggregation in MongoDB/src/main/java/Loader.java
@@ -1,65 +1,17 @@
 
-import static utils.MongoDB.addProducts;
-import static utils.MongoDB.addProductsToShop;
-import static utils.MongoDB.addShop;
-import static utils.MongoDB.printStatistic;
-import static utils.MongoDB.shutdownDB;
-import static utils.MongoDB.split;
 
-import utils.TestData;
 
-import java.io.BufferedReader;
+import utils.MongoDB;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Loader {
 
-    private static String commandExample = "Примеры команд:"
-            + "\n - ДОБАВИТЬ_МАГАЗИН Девяточка"
-            + "\n - ДОБАВИТЬ_ТОВАР Вафли 54"
-            + "\n - ВЫСТАВИТЬ_ТОВАР Вафли Девяточка"
-            + "\n - СТАТИСТИКА_ТОВАРОВ"
-            + "\n - EXIT - завершает работу программы";
-
-
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        TestData.insertTestData();
-        System.out.println(commandExample);
-
-        for (;;) {
-            System.out.println("Введите команду:");
-            String[] input = split(reader.readLine().trim());
-
-            String instruction = input[0];
-            String object = input.length > 1 ? input[1] : "";
-
-            switch (instruction) {
-                case "ДОБАВИТЬ_МАГАЗИН":
-                    addShop(object);
-                    break;
-                case "ДОБАВИТЬ_ТОВАР":
-                    addProducts(object);
-                    break;
-                case "ВЫСТАВИТЬ_ТОВАР":
-                    addProductsToShop(object);
-                    break;
-                case "СТАТИСТИКА_ТОВАРОВ":
-                    printStatistic();
-                    break;
-                case "EXIT":
-                    shutdownDB();
-                    return;
-                default:
-                    printError();
-            }
-        }
+       new MongoDB().start();
     }
 
-    private static void printError() {
-        System.out.println("Неверный ввод!");
-        System.out.println(commandExample);
-    }
+
 
 
 }
--- Module 13 - Redis and MongoDB databases/13.4-Data aggregation in MongoDB/src/main/java/utils/MongoDB.java
+++ Module 13 - Redis and MongoDB databases/13.4-Data aggregation in MongoDB/src/main/java/utils/MongoDB.java
@@ -9,8 +9,7 @@ import static com.mongodb.client.model.Aggregates.group;
 import static com.mongodb.client.model.Aggregates.lookup;
 import static com.mongodb.client.model.Aggregates.match;
 import static com.mongodb.client.model.Aggregates.unwind;
-import static com.mongodb.client.model.Filters.eq;
-import static com.mongodb.client.model.Filters.lt;
+import static com.mongodb.client.model.Filters.*;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
@@ -18,147 +17,154 @@ import com.mongodb.client.AggregateIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.bson.io.BsonOutput;
+import org.w3c.dom.ls.LSOutput;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.DuplicateFormatFlagsException;
 
 public class MongoDB {
 
-    private static MongoClient mongoClient;
-    private static MongoDatabase stores;
-    private static MongoCollection<Document> shops;
-    private static MongoCollection<Document> products;
+    private final String HOST_MONGO_CLIENT = "127.0.0.1";
+    private final int PORT_MONGO_CLIENT = 27017;
+    private final  String DATABASE_NAME = "local";
 
-    static {
-        mongoClient = new MongoClient("127.0.0.1", 27017);
-        stores = mongoClient.getDatabase("local");
-        shops = stores.getCollection("shop");
-        products = stores.getCollection("products");
-    }
+    private MongoClient mongoClient = new MongoClient(HOST_MONGO_CLIENT, PORT_MONGO_CLIENT);
+    private MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
+
+    private MongoCollection<Document> shops = database.getCollection("shop");
+    private MongoCollection<Document> products  = database.getCollection("products");
+
+    //------------------------------------------------------------------------------------------
+
+    public void start() {
+         String commandExample = "Примеры команд:"
+                + "\n - ДОБАВИТЬ_МАГАЗИН Девяточка"
+                + "\n - ДОБАВИТЬ_ТОВАР Вафли 54"
+                + "\n - ВЫСТАВИТЬ_ТОВАР Вафли Девяточка"
+                + "\n - СТАТИСТИКА_ТОВАРОВ"
+                + "\n - EXIT - завершает работу программы";
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(commandExample);
+
+        for (;;) {
+            System.out.println("Введите команду:");
+            try {
+                String[] input = reader.readLine().split(" ");
+
+                String instruction = input[0];
+                String stringInput = parseString(input).toLowerCase();
 
 
-    public static void addShop(String shopName) {
-        if (shopName.isEmpty()) {
-            throw new RuntimeException();
-        }
+                if (instruction.equalsIgnoreCase("ДОБАВИТЬ_МАГАЗИН")) {
+                    addShops(stringInput);
+                } else if (instruction.equalsIgnoreCase("ДОБАВИТЬ_ТОВАР")) {
+                    addProducts(stringInput);
+                } else if (instruction.equalsIgnoreCase("ВЫСТАВИТЬ_ТОВАР")) {
+                    displayProduct(stringInput);
+                } else if (instruction.equalsIgnoreCase("СТАТИСТИКА_ТОВАРОВ")) {
+                    printStatisticsAndAggregate(products);
+                } else if (instruction.equalsIgnoreCase("EXIT")) {
+                    exitDB();
+                    break;
+                } else System.out.println("Неверный ввод! \n" + commandExample);
 
-        try {
-            Document shop = new Document("name", shopName);
-            shop.append("products", new ArrayList<String>());
-
-            if (getShop(shopName) == null) {
-                MongoDB.shops.insertOne(shop);
-                System.out.println("Магазин " + shopName + " добавлен!");
-            } else {
-                System.out.println("Магазин уже был добавлен!");
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            System.out.println("Неверный ввод!");
         }
     }
 
-    public static void addProducts(String input) {
-        try {
-            String[] var = splitProductNameAndPrice(input);
-            String name = var[1];
-            Document product = new Document("name", name);
-            product.append("price", Integer.parseInt(var[2]));
+    private String parseString (String [] strings) {
+        String  fString = "";
 
-            if (getProduct(name) == null) {
-                products.insertOne(product);
-                System.out.println("Продукт " + name + " добавлен!");
-            } else {
-                System.out.println("Товар уже был добавлен");
-            }
-        } catch (Exception e) {
-            System.out.println("Неверный ввод!");
+        if (strings.length == 3 ) fString = strings[1] + " " + strings[2];      // + " " + strings[2];
+        else if (strings.length == 2) fString = strings[1];                     // + " " + strings[1];
+        else  fString = "";
+        return fString;
+    }
+
+    private void addShops(String nameShop) {
+        Document shop = new Document("name", nameShop);
+        shop.append("products", new ArrayList<String>());
+
+        if (getShop(nameShop) == null) {
+            shops.insertOne(shop);
+            System.out.println("Магазин " + nameShop.toUpperCase() + " добавлен.");
+
+        } else System.out.println("ВНИМАНИЕ - Магазин " + nameShop.toUpperCase() + " уже был добавлен ранее");
+    }
+
+    private void addProducts(String nameAndPriceProduct) {
+        String[] namePriceProduct = nameAndPriceProduct.split(" ");
+        String nameProduct = namePriceProduct[0];
+        int priceProduct = Integer.parseInt(namePriceProduct[1]);
+
+        Document product = new Document("name", nameProduct);
+        product.append("price", priceProduct);
+
+        if (getProduct(nameProduct) == null) {
+            products.insertOne(product);
+            System.out.println("Продукт " + nameProduct.toUpperCase() + " добавлен по цене " +  priceProduct);
+
+        } else System.out.println("Продукт " + nameProduct.toUpperCase() + " уже был добавлен");
+    }
+
+    private void displayProduct(String nameProductAndNameShop) {
+        String[] namePrAndNameSh = nameProductAndNameShop.split(" ");
+        String nameProduct = namePrAndNameSh[0];
+        String nameShop = namePrAndNameSh[1];
+
+            shops.updateOne(eq(getShop(nameShop)), new Document("$addToSet",
+                    new Document("products", getProduct(nameProduct).get("name"))));
+
+            System.out.println("Продукт " + nameProduct.toUpperCase() + " добавлен в магазин " + nameShop.toUpperCase());
+    }
+
+    private void printStatisticsAndAggregate (MongoCollection<Document> collection ) {
+
+        AggregateIterable<Document> aggregateProduct = collection.aggregate(Arrays.asList(
+                                         lookup("shop", "name", "products", "shop_list"),
+                                         unwind("$shop_list"),
+                                         group("$name", sum("count_products", 1),
+                                                            min("min_price", "$price"),
+                                                            max("max_price", "$price"),
+                                                            avg("avg_price" , "$price")),
+                                         match(lt("cheap_product", 100)),
+                                         count("cheap_product")));
+
+        System.out.println("В базе:" + "\n\t Магазинов - " + shops.countDocuments() +
+                                       "\n\t Товаров - " + products.countDocuments());
+
+        for (Document document : aggregateProduct) {
+            String shopName = (String) document.get("_id");
+            System.out.println("Магазин " + shopName);
+            System.out.println("Количество товара: " + document.get("count_products"));
+            System.out.println("Средняя цена товара: " + document.get("avg_price"));
+            System.out.println("Самый дорогой товар:  " + document.get("max_price"));
+            System.out.println("Самый дешевый товар:  " + document.get("min_price"));
+            System.out.println("Количество товаров, дешевле 100 рублей: " + document.get("cheap_product"));
         }
+
     }
 
-    public static void addProductsToShop(String input) {
-        try {
-            String[] var = splitProductNameAndPrice(input);
-            String shopName = var[2];
-            String productName = var[1];
-            shops.updateOne(eq(getShop(shopName)), new Document("$addToSet",
-                    new Document("products", getProduct(productName).get("name"))));
-            System.out.println("Продукт " + productName + " добавлен в магазин " + shopName);
-        } catch (MongoException e) {
-            System.out.println("Магазин или продукт не существуют! ");
-            System.out.println(e);
-        }
-    }
-
-    public static void printStatistic() {
-        printInfo(getAggregate());
-        printInfoCheapProduct(cheapestProductCount());
-    }
-
-    private static AggregateIterable<Document> getAggregate() {
-        return products.aggregate(
-                Arrays.asList(
-                        lookup("shop", "name", "products", "shop_list"),
-                        unwind("$shop_list"),
-                        group("$shop_list.name",
-                                sum("count_products", 1),
-                                min("min_price", "$price"),
-                                max("max_price", "$price"),
-                                avg("avg_price", "$price"))
-                )
-        );
-    }
-
-    private static void printInfo(AggregateIterable<Document> documents) throws MongoException {
-        try {
-            for (Document document : documents) {
-                String shopName = (String) document.get("_id");
-                System.out.println("Магазин " + shopName);
-                System.out.println("Количество товара: " + document.get("count_products"));
-                System.out.println("Средняя цена товара: " + document.get("avg_price"));
-                System.out.println("Самый дорогой товар:  " + document.get("max_price"));
-                System.out.println("Самый дешевый товар:  " + document.get("min_price"));
-            }
-        } catch (MongoException e) {
-            System.out.println(e);
-        }
-    }
-
-    protected static void printInfoCheapProduct(AggregateIterable<Document> documents) throws MongoException {
-        try {
-            for (Document document : documents) {
-                System.out.println("Количество товаров, дешевле 100 рублей: " + document.get("cheap_product"));
-            }
-        } catch (MongoException e) {
-            System.out.println(e);
-        }
-    }
-
-    protected static AggregateIterable<Document> cheapestProductCount() {
-        return products.aggregate(Arrays.asList(
-                match(lt("cheap_product", 100)),
-                count("cheap_product")));
-    }
-
-    public static void shutdownDB() {
-        stores.drop();
+    private void exitDB() {
+        database.drop();
         mongoClient.close();
         System.out.println("Работа завершена!");
     }
 
-    private static Document getShop(String name) {
+    private Document getShop(String name) {
         return shops.find(new Document("name", name)).first();
     }
 
-    private static Document getProduct(String name) {
+    private Document getProduct(String name) {
         return products.find(new Document("name", name)).first();
-    }
-
-    public static String[] split(String var) {
-        return var.split(" ", 2);
-    }
-
-    public static String[] splitProductNameAndPrice(String var) {
-        return var.split(" ", 3);
     }
 
 
